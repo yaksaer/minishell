2858a4293a82936219fddfde518687c8ae88282a
Carlo Bilbo <cbilbo@mi-m5.msk.21-school.ru>
2021-09-28 18:05:46 +0300

    add realloc

---
+++ minishell/ms/libft/ft_realloc.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_realloc.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/28 14:08:50 by marvin            #+#    #+#             */
+/*   Updated: 2021/09/28 15:03:56 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_realloc(void *ptr, size_t size)
+{
+	void	*result;
+	size_t	ptr_size;
+
+	if (!ptr)
+		return(malloc(size))
+	ptr_size = sizeof(ptr);
+	if (size <= ptr_size)
+		return (ptr);
+	result = (void *)malloc(size);
+	if (!result)
+		return (NULL);
+	result = ft_memset(result, 0, size);
+	result = ft_memcpy(result, str, ptr_size);
+	free(str);
+	return (result);
+}
