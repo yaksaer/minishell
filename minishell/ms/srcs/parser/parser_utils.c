6ee9adf27b2cf9794a6d678574fbdd99e3784d18
Ярослав Лапундяк <yara-l@mail.ru>
2021-10-15 15:40:07 +0300

    try to find exit

--- minishell/ms/srcs/parser/parser_utils.c
+++ minishell/ms/srcs/parser/parser_utils.c
@@ -6,7 +6,7 @@
 /*   By: cbilbo <cbilbo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/14 19:51:49 by cbilbo            #+#    #+#             */
-/*   Updated: 2021/10/14 20:22:28 by cbilbo           ###   ########.fr       */
+/*   Updated: 2021/10/15 14:39:51 by cbilbo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,3 +23,26 @@ char	*ft_add_char(char *string, char c)
 	return (res);
 }
 
+void	print_commands(t_main *main)
+{
+	t_commands	*temp;
+	int			i;
+	int			c;
+
+	temp = main->commands;
+	i = 0;
+	while (temp != NULL)
+	{
+		c = -1;
+		printf("\n%d pipe\n\ncmds:\n", i);
+		while (temp->cmd && temp->cmd[++c] != NULL)
+			printf("%s\n", temp->cmd[c]);
+		printf("\nredirects:\n");
+		c = -1;
+		while (temp->redir && temp->redir[++c] != NULL)
+			printf("%s\n", temp->redir[c]);
+		printf("\ninput = %d, output = %d\n", temp->input, temp->output);
+		temp = temp->next;
+		i++;
+	}
+}
